edcff623115739ba8d7a355bf2b61478455ec15f
Jonathan Hew <jonathanhew@Jonathans-MacBook-Pro.local>
2022-05-14 16:36:59 +0100

    react states added for all inputs

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 //test comment
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  //states for each input 
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  
+
   return (
     <div className="App">
       <div className='information'>
         <label>Name:</label>
-        <input type = "text"/>
+        <input type = "text" onChange={(event) => {setName(event.target.value)}}/>
         <label>Age:</label>
-        <input type = "number"/>
+        <input type = "number" onChange={(event) => {setAge(event.target.value)}}/>
         <label>Country:</label>
-        <input type = "text"/>
+        <input type = "text" onChange={(event) => {setCountry(event.target.value)}}/>
         <label>Position:</label>
-        <input type = "text"/>
+        <input type = "text" onChange={(event) => {setPosition(event.target.value)}}/>
         <label>Wage (year):</label>
-        <input type = "number"/>
+        <input type = "number" onChange={(event) => {setWage(event.target.value)}}/>
         <button>Add Employee</button>
       </div>
     </div>

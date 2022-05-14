6bd5e1476987a614ab9039bd7cf85ddb422bbc5f
Jonathan Hew <jonathanhew@Jonathans-MacBook-Pro.local>
2022-05-14 16:16:51 +0100

    inputs created and styled slightly with css

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='information'>
+        <label>Name:</label>
+        <input type = "text"/>
+        <label>Age:</label>
+        <input type = "number"/>
+        <label>Country:</label>
+        <input type = "text"/>
+        <label>Position:</label>
+        <input type = "text"/>
+        <label>Wage (year):</label>
+        <input type = "number"/>
+        <button>Add Employee</button>
+      </div>
     </div>
   );
 }

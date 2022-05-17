b74d89879c0682e49ba818a4821f21e78c689d16
Jonathan Hew <jonathanh9655@hotmail.com>
2022-05-17 21:12:17 +0100

    show all employees button added but not implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 //test comment
-import './App.css';
+import "./App.css";
 import { useState } from "react";
-import Axios from 'axios';
+import Axios from "axios";
 
 function App() {
-  //states for each input 
+  //states for each input
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
   const [country, setCountry] = useState("");
@@ -13,31 +13,60 @@ function App() {
 
   const addEmployee = () => {
     //send info from frontend to backend
-    Axios.post('http://localhost:3001/create', {
-      name: name, 
-      age: age, 
-      country: country, 
-      position: position, 
-      wage: wage
-    }).then(()=> {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
       console.log("success");
     });
   };
 
   return (
     <div className="App">
-      <div className='information'>
+      <div className="information">
         <label>Name:</label>
-        <input type = "text" onChange={(event) => {setName(event.target.value)}}/>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
         <label>Age:</label>
-        <input type = "number" onChange={(event) => {setAge(event.target.value)}}/>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
         <label>Country:</label>
-        <input type = "text" onChange={(event) => {setCountry(event.target.value)}}/>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
         <label>Position:</label>
-        <input type = "text" onChange={(event) => {setPosition(event.target.value)}}/>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
         <label>Wage (year):</label>
-        <input type = "number" onChange={(event) => {setWage(event.target.value)}}/>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
         <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      -----------------------------------------------------------------------------------------------------
+      <div className="employees">
+        <button>Show Employees</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,43 +1,42 @@
-const express = require ('express')
-const app = express()
-const mysql = require ('mysql')
-const cors = require('cors');
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 //connecting to the db
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: '38SorrelHeath',
-    database: 'employeeSystem'
+  user: "root",
+  host: "localhost",
+  password: "38SorrelHeath",
+  database: "employeeSystem",
 });
 
-app.post('/create', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    const country = req.body.country;
-    const position = req.body.position;
-    const wage = req.body.wage;
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
 
-    //inserting into mysql db
-    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)', 
-    [name, age, country, position, wage], 
+  //inserting into mysql db
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
     (err, result) => {
-        if (err) {
-            console.log(err)
-        }//end if
-        else {
-            //success message to know if addded into DB
-            res.send("Values Inserted.")
-        }
+      if (err) {
+        console.log(err);
+      } //end if
+      else {
+        //success message to know if addded into DB
+        res.send("Values Inserted.");
+      }
     }
-    );
+  );
 });
 
-
-app.listen(3001, ()=> {
-    console.log("Yay, your server is running on port 3001.")
+app.listen(3001, () => {
+  console.log("Yay, your server is running on port 3001.");
 });
-
